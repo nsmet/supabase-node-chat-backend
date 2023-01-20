07279ddf46ae372fe8e6584b3e61c4d8b40b9610
Nick Smet <nick@leika.io>
2023-01-20 13:25:52 +0100

    types added for conversation / user / message

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,28 @@ export interface TypedRequestBody<T> extends Express.Request {
   export interface TypedRequestQuery<T> extends Express.Request {
     query: T
   }
+
+  export interface User {
+    id: string;
+    username: string;
+    created_at: string;
+  }
+
+  export interface Conversation {
+    id: string;
+    name: string;
+    user_owner_id: string;
+    created_at: string;
+  }
+
+  export interface Message {
+    id: string;
+    user_id: string;
+    message: string;
+    created_at: string;
+  }
+
+  export interface UserConversation {
+    user_id: string;
+    conversation_id: string;
+  }
