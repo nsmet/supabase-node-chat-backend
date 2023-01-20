19461a601528c1f2567e16c91a28646daf1d43d8
Nick Smet <nick@leika.io>
2023-01-20 12:49:13 +0100

    working create user endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import { randomUUID } from "crypto";
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.SUPABASE_PROJECT_URL as string, process.env.SUPABASE_PUBLIC_ANON as string);
@@ -16,9 +17,22 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
  
+// CREATE A NEW USER
 app.post("/create-user", async function (req: TypedRequestBody<{username: string}>, res: Response) {
-  // CREATE A NEW USER
-  console.log(req.body);
+  const { data, error } = await supabase
+    .from('users')
+    .upsert({ 
+      id: randomUUID(),
+      username: req.body.username,
+      created_at: new Date().toLocaleString()
+     })
+    .select()
+
+    if (error) {
+      res.send(500)
+    } else {
+      res.send(data[0])
+    }
   
 });
 app.listen(3000);
