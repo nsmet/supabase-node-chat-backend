7f6670778e2d8d585cf9d935630731ca35df25bb
Nick Smet <nick@leika.io>
2023-01-20 11:54:52 +0100

    create a supabase client nodemon for auto refresh on change

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.SUPABASE_PROJECT_URL as string, process.env.SUPABASE_PUBLIC_ANON as string);
 const app = express();
  
 app.get("/", function (req, res) {
